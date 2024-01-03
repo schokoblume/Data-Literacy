bfb5eda814a0f55fc721437cb268e2be94ed5991
Xenia <xenia.rieger24@gmail.com>
2024-01-03 14:07:15 +0100

    data preperation

--- Daten/countKeywordsToCSV.py
+++ Daten/countKeywordsToCSV.py
@@ -4,10 +4,13 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 # Before you run this script, download articles with getArticles.py
+# You could do everything in one function, but we already had the intermediate results, so we added functions to work on them
 
 # Keywords determine if an article is about war. At least one of them has to be in the headline to count the article.
 # In-word is searched, i.e. for keyword "krieg", "Angriffskrieg" is counted
 keywords = ["krieg", "Krieg", "konflikt", "Konflikt", "Angriff", "angriff"]
+
+directory = os.getcwd() + "\\spiegel_articles_ausland_politik"
 
 # create a data frame with row for each headline: date, each keyword (present: 1, not present: 0), war: at least one keyword present (yes: 1, no: 0)
 # output_file: name of output file
@@ -81,6 +84,46 @@ def create_accumulated_csv(input_file, output_file, overwrite=False):
     # save as csv
     new_df.to_csv(output_file, sep=";")
 
-#create_basic_csv("Spiegel_articles_keywords_ausland_poliktik.csv", keywords, os.getcwd() + "\\spiegel_articles_ausland_politik")
+# create a data frame with row for each date: how many articles were about war, how many articles on that day, standardized number
+# input_file: csv file created with create_accumulated_csv
+# output_file: name of output file
+# directory: same as in create_basic_csv
+# overwirte: True, if file exists it will be overwritten, False, if file exists we do not change it
+def create_standardized_csv(input_file, output_file, directory, overwrite=False):
+    if not overwrite:
+        if os.path.exists(output_file):
+            print("Output file already exists. Change file name or enable overwrite.")
+    if not os.path.exists(input_file):
+        print("The specified input file does not exist.")
 
+    base_df = pd.read_csv(input_file, sep=";")
+    new_df = pd.DataFrame(columns=["date", "war articles", "total articles", "standardized"])
+
+    # take war articles from previous df
+    new_df["date"] = base_df["date"]
+    new_df["war articles"] = base_df["war articles"]
+
+    # go through files to find total article number
+    for file in Path(directory).glob('**/*.txt'):
+        filename = os.path.basename(file).split('/')[-1]
+        date = filename.replace(".txt", "")
+        # show progress
+        #print(date)
+        lines = open(file, encoding="utf-8").readlines()
+
+        counter = 0
+        for line in lines:
+            counter += 1
+        new_df.loc[new_df['date'] == date, "total articles"] = counter
+        # standardize
+        war_num = new_df.loc[new_df['date'] == date, "war articles"]
+        new_df.loc[new_df['date'] == date, "standardized"] = war_num / counter
+
+    # save as csv
+    new_df.to_csv(output_file, sep=";")
+
+
+#create_basic_csv("Spiegel_articles_keywords_ausland_poliktik.csv", keywords, directory)
 #create_accumulated_csv("Spiegel_articles_keywords_ausland_politik.csv", "Spiegel_articles_accumulated_ausland_politik.csv")
+#create_standardized_csv("Spiegel_articles_accumulated_ausland_politik.csv", "spiegel_articles_standardized.csv", directory)
+
---
+++ Daten/joinedData.py
@@ -0,0 +1,57 @@
+import datetime
+import pandas as pd
+import os
+import numpy as np
+
+# join survey values and standardized war article number in one csv
+# problem: they have different times, survey only every couple weeks
+# timeframe: how many days previous to survey should be used
+
+def join_data(survey_input, article_input, output_file, timeframe, overwrite = False):
+	if not overwrite:
+		if os.path.exists(output_file):
+			print("Output file already exists. Change file name or enable overwrite.")
+	if not os.path.exists(survey_input):
+		print("The specified survey input file does not exist.")
+	if not os.path.exists(article_input):
+		print("The specified article input file does not exist.")
+
+	survey_df = pd.read_csv(survey_input, sep=";", encoding="latin1")
+	article_df = pd.read_csv(article_input, sep=";")
+	survey_df["standardized_war_articles"] = 0
+
+	for date_str in survey_df["date"]:
+		date = datetime.datetime.strptime(date_str, '%d.%m.%Y').date()
+		counter = timeframe
+		war_num = 0
+		# sum up standardized war article number for days defined by timeframe before the publish date of survey
+		while(counter > 0):
+			counter -= 1
+			# do not include publish date of survey
+			date -= datetime.timedelta(days=1)
+			date_index = date.strftime('%d') + "." + date.strftime('%m') + "." + date.strftime('%Y')
+			war_this_date = article_df.loc[article_df["date"] == date_index, "standardized"].item()
+			war_num += war_this_date
+
+		survey_df.loc[survey_df["date"] == date_str, "standardized_war_articles"] = war_num / timeframe
+
+	# save as csv
+	survey_df.to_csv(output_file, sep=";")
+
+# use median lenght between surveys as timeframe
+def median_lenght(survey_input):
+	survey_df = pd.read_csv(survey_input, sep=";", encoding="latin1")
+	diffs = []
+	prev_date = None
+	for date_str in survey_df["date"]:
+		date_now = datetime.datetime.strptime(date_str, '%d.%m.%Y').date()
+		if not prev_date == None:
+			difference = prev_date - date_now
+			diffs.append(difference.days)
+		prev_date = date_now
+
+	return np.median(diffs)
+
+#join_data("survey_values.csv", "spiegel_articles_standardized.csv", "joined_survey_article_standardized_median.csv", median_lenght("survey_values.csv"))
+#join_data("survey_values.csv", "spiegel_articles_standardized.csv", "joined_survey_article_standardized_oneweek.csv", 7)
+#join_data("survey_values.csv", "spiegel_articles_standardized.csv", "joined_survey_article_standardized_twoweeks.csv", 14)
